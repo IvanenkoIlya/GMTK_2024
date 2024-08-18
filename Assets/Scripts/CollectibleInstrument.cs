f2fcb76eb1d03cb148192d2630c9f883aa511f39
IvanenkoIlya <IvanenkoIlya@users.noreply.github.com>
2024-08-18 12:23:49 -0700

    Changed followers to use nav mesh

--- Assets/Scripts/CollectibleInstrument.cs
+++ Assets/Scripts/CollectibleInstrument.cs
@@ -2,37 +2,26 @@ using UnityEngine;
 
 public class CollectibleInstrument : MonoBehaviour
 {
-   public Sprite FollowerSprite;
-   public GameObject Follower;
-   public GameObject FollowerTarget;
+   public GameObject FollowerPrefab;
 
    // Start is called once before the first execution of Update after the MonoBehaviour is created
-   void Start()
-   {
-
-   }
+   void Start() { }
 
    // Update is called once per frame
-   void Update()
-   {
-
-   }
+   void Update() { }
 
    private void OnTriggerEnter2D(Collider2D collision)
    {
-      print("Collided!");
-      if (collision.gameObject.tag == "Player")
+      if(collision.gameObject.CompareTag("Player"))
       {
-         var temp = Instantiate(Follower, transform.position, Quaternion.identity);
-         var tempTarget = FollowerTarget.GetComponent<leader>().FollowerAlly;
-         //Attempting to get the follower chain to work
-         while (tempTarget != null)
-         {
-            FollowerTarget = FollowerTarget.GetComponent<leader>().FollowerAlly;
-            tempTarget = FollowerTarget.GetComponent<leader>().FollowerAlly;
-         }
-         temp.GetComponent<Follower>().Target = FollowerTarget;
-         FollowerTarget.GetComponent<leader>().FollowerAlly = temp;
+         var target = collision.gameObject;
+
+         while(target.GetComponent<Leader>().Followers.Count > 0)
+            target = target.GetComponent<Leader>().Followers[0];
+
+         var follower = Instantiate(FollowerPrefab, transform.position, Quaternion.identity);
+         target.GetComponent<Leader>().AssignFollower(follower);
+         follower.GetComponent<Follower>().Target = target;
 
          Destroy(gameObject);
       }
